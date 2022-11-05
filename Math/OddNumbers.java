605e738d69706115511976068494fe39ebc450b4
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-11-05 13:46:45 +0530

    Create OddNumbers.java

---
+++ Math/OddNumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class OddNumbers {
+    // NOTE: Please do not modify this function
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<Integer> result = oddNumbers(n);
+
+        for (int j : result)
+            System.out.print(j + " ");
+
+    }
+
+    // TODO: Implement this method
+    static ArrayList<Integer> oddNumbers(int n) {
+    }
+}
