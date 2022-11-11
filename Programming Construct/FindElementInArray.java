0df0f34a0bc41410c1a6a3fa87845af072ab1ff5
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-11-11 17:22:31 +0530

    Create FindElementInArray.java

---
+++ Programming Construct/FindElementInArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class FindElement {
+    // NOTE: Please do not modify this function
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int x = sc.nextInt();
+
+        int result = findElement(n, arr, x);
+
+        System.out.println(result);
+
+    }
+
+    // TODO: Implement this method
+    static int findElement(int n, int[] arr, int x) 
+    {
+            for(int i=0 ; i<n ; i++){
+                if(arr[i] == x){
+                    return i;
+                }
+                
+            }
+            return -1;
+
+    }
+}
